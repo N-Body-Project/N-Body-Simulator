cd5aaf8ec7fec19332fc695bd703feab92d00960
Artem Buriak <44706581+RodneyMcKey@users.noreply.github.com>
2024-12-28 21:52:08 +0200

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -3,13 +3,22 @@ Write-Host "||Checking if rustfmt is installed..." -ForegroundColor Yellow
 if (-not (Get-Command rustfmt -ErrorAction SilentlyContinue)) {
     Write-Host "||rustfmt is not installed. Please run 'rustup component add rustfmt' to install it." -ForegroundColor Red
     exit 1
+}
+
+# Build the project
+Write-Host "||Building debug build..." -ForegroundColor Yellow
+cargo build
+if ($LASTEXITCODE -eq 0) {
+    Write-Host "||Build successful. Running the project..." -ForegroundColor Green -BackgroundColor Green
+    & "target/debug/n-body-simulator.exe"
 } else {
-    Write-Host "||rustfmt is installed." -ForegroundColor Green
+    Write-Host "||Build failed." -ForegroundColor Red -BackgroundColor Red
+    exit 1
 }
 
 # Format Rust code
 Write-Host "||Formatting Rust code..." -ForegroundColor Yellow
-cargo fmt
+cargo fmt --all -- --check
 if ($LASTEXITCODE -eq 0) {
     Write-Host "||Formatting successful." -ForegroundColor Green
 } else {
@@ -21,16 +30,3 @@ if ($LASTEXITCODE -eq 0) {
 # MEMO: Uknown behaviour, adjust as we go.
 Write-Host "||Running clippy..." -ForegroundColor Yellow
 cargo clippy
-
-# Build the project
-Write-Host "||Building debug build..." -ForegroundColor Yellow
-
-# Run cargo build and capture its exit code
-cargo build
-if ($LASTEXITCODE -eq 0) {
-    Write-Host "||Build successful. Running the project..." -ForegroundColor Green -BackgroundColor Green
-    & "target/debug/n-body-simulator.exe"
-} else {
-    Write-Host "||Build failed." -ForegroundColor Red -BackgroundColor Red
-    exit 1
-}
