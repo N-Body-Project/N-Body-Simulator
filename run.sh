f7fae9656a9c67f884b87261cdfa38424b74b685
Artem Buriak <44706581+RodneyMcKey@users.noreply.github.com>
2024-12-28 22:22:03 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,24 +11,12 @@ echo -e "${YELLOW}||Checking if rustfmt is installed...${RESET}"
 if ! command -v rustfmt &> /dev/null; then
     echo -e "${RED}||rustfmt is not installed. Please run 'rustup component add rustfmt' to install it.${RESET}"
     exit 1
-else
-    echo -e "${GREEN}||rustfmt is installed.${RESET}"
-fi
-
-# Format Rust code
-echo -e "${YELLOW}||Formatting Rust code...${RESET}"
-cargo fmt
-if [ $? -eq 0 ]; then
-    echo -e "${GREEN}||Formatting successful.${RESET}"
-else
-    echo -e "${RED}||Formatting failed.${RESET}"
-    exit 1
 fi
 
 # Clippy
 # MEMO: Unknown behavior, adjust as we go.
 echo -e "${YELLOW}||Running clippy...${RESET}"
-cargo clippy
+cargo clippy --all-targets --all-features
 
 # Build the project
 echo -e "${YELLOW}||Building debug build...${RESET}"
@@ -38,5 +26,15 @@ if [ $? -eq 0 ]; then
     ./target/debug/n-body-simulator
 else
     echo -e "${RED}||Build failed.${RESET}"
+    exit 1
+fi
+
+# Format Rust code
+echo -e "${YELLOW}||Formatting Rust code...${RESET}"
+cargo fmt --all -- --check
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}||Formatting successful.${RESET}"
+else
+    echo -e "${RED}||Formatting failed.${RESET}"
     exit 1
 fi
