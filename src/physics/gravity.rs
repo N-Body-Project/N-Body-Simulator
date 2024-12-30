76123214e6046b0e6e9a99c51a8907750d299e0b
Maksym Prymierov <primerovmax@gmail.com>
2024-12-30 03:16:53 +0200

    [NBS-15] feat(physics): introduce gravity module and implement gravitational_force()
    Reason:
    - Added a dedicated module for physics calculations.
    - Implemented the gravitational force formula (F = G * (m1 * m2) / r^2)
    - Prevent division by zero by returning zero vector if distance is too small.
    - Facilitates clearer separation of physics-related code.
    
    Changes:
    - Created `src/physics/gravity.rs` with gravitational_force() function.
    - Added `src/physics/mod.rs` to export the new gravity module.
    - Used `vecmath` for vector operations (subtraction, normalization, scaling).
    
    Signed-off-by: Maksym Prymierov <primerovmax@gmail.com>

---
+++ src/physics/gravity.rs
@@ -0,0 +1,32 @@
+use crate::types::particle::Particle;
+use vecmath::Vector3;
+use vecmath::vec3_sub;
+use vecmath::vec3_square_len;
+use vecmath::vec3_normalized;
+use vecmath::vec3_scale;
+
+// gravitational constant in SI units
+pub const G: f64 = 6.67430e-11;
+
+// The force of interaction between two bodies is directly proportional to the mass of each body F = G * (m1 * m2) / r^2 
+pub fn gravitational_force(p1: &Particle, p2: &Particle) -> Vector3<f64> {
+    // vector from p1 to p2
+    let r_vec = vec3_sub::<f64>(p2.pos(), p1.pos());
+
+    // Square of the distance between two particles
+    let distance_sq = vec3_square_len::<f64>(r_vec);
+
+    // Return a zero vector to avoid division by zero
+    if distance_sq < f64::EPSILON {
+        return [0.0, 0.0, 0.0];
+    }
+
+    // Gravity force betweed two particles
+    let force_mag = G * (p1.mass() * p2.mass()) / distance_sq;
+
+    // Force direction
+    let direction = vec3_normalized::<f64>(r_vec);
+
+    // Force vector for p1
+    vec3_scale::<f64>(direction, force_mag)
+}
