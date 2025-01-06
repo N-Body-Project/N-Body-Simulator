d05213cdda096549d5912dff35d6e5065d98d4b3
Maksym Prymierov <primerovmax@gmail.com>
2025-01-06 03:27:46 +0200

    [NBS-33] refactor(main): streamline particle initialization and add timing for force computation
    - Simplified particle initialization using a loop with dynamically assigned positions and masses.
    - Added timing using `Instant::now()` to measure the duration of force computation and particle updates.
    - Replaced hardcoded particle details with a scalable approach based on `particle_count`.
    - Updated the force application loop to use `enumerate()` for clarity.
    - Improved output to include total execution time for better performance evaluation.
    
    Signed-off-by: Maksym Prymierov <primerovmax@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,37 @@
 use crate::types::nbodysystem::NBodySystem;
 use crate::types::particle::Particle;
+use std::time::Instant;
 
 pub mod physics;
 pub mod types;
 
 fn main() {
     let mut system = NBodySystem::default();
+    let particle_count: usize = 3;
 
-    const PARTICLE_MASS: f64 = 1.5e14;
-    system.add_particle(Particle::new(
-        0,
-        [-50.0, 0.0, 0.0],
-        [0.0, 0.0, 0.0],
-        [0.0, 0.0, 0.0],
-        PARTICLE_MASS,
-    ));
-    system.add_particle(Particle::new(
-        1,
-        [50.0, 0.0, 0.0],
-        [0.0, 0.0, 0.0],
-        [0.0, 0.0, 0.0],
-        PARTICLE_MASS,
-    ));
-    system.add_particle(Particle::new(
-        2,
-        [100.0, 0.0, 0.0],
-        [0.0, 0.0, 0.0],
-        [0.0, 0.0, 0.0],
-        PARTICLE_MASS,
-    ));
+    for i in 0..particle_count {
+        system.add_particle(Particle::new(
+            i as u64,
+            [i as f64 * 50.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0],
+            1.5e14 * (i + 1) as f64,
+        ));
+    }
+
+    let start = Instant::now();
 
     let forces = system.compute_all_forces();
     println!("{forces:?}");
 
-    for (i, force) in forces.iter().enumerate().take(3) {
+    for (i, force) in forces.iter().enumerate().take(particle_count) {
         let p = system.get_particle_by_index(i).expect("REASON");
         p.update_particle_euler(*force, 0.1);
         println!("Velocity result {:?}", p.velocity());
         println!("     Pos result {:?}", p.pos());
     }
+
+    let duration = start.elapsed();
+
+    println!("Total Time: {duration:#?}.");
 }
