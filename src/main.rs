2c075f9b4eeecb956b72cf07365c4ab9e7a8b31a
Maksym Prymierov <primerovmax@gmail.com>
2025-07-22 22:27:53 +0300

    [NBS-37] feat: add real-time visualization of N-body simulation using macroquad
    - Added `macroquad` dependency for graphical rendering.
    - Converted `main` to an async macroquad entrypoint.
    - Replaced previous terminal-based simulation with real-time visualization.
    - Initialized 3 main particles with distinct masses, colors, and velocities.
    - Added 1000 smaller
    
    Signed-off-by: Maksym Prymierov <primerovmax@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,79 @@
+extern crate rand;
+
 use crate::types::nbodysystem::NBodySystem;
 use crate::types::particle::Particle;
-use std::time::Instant;
+use macroquad::prelude::*;
 
 pub mod physics;
 pub mod types;
 
-fn main() {
+#[macroquad::main("N Body Problem")]
+async fn main() {
     let mut system = NBodySystem::default();
-    let particle_count: usize = 3;
+    let particle_count: usize = 1003;
 
-    for i in 0..particle_count {
+    system.add_particle(Particle::new(
+        0,
+        [1000.0, 500.0, 0.0],
+        [0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0],
+        100000000000.0,
+    ));
+
+    system.add_particle(Particle::new(
+        1,
+        [500.0, 500.0, 0.0],
+        [-0.0000, 0.1, 0.0],
+        [0.0, 0.0, 0.0],
+        50000000.0,
+    ));
+
+    system.add_particle(Particle::new(
+        2,
+        [1500.26, 1200.0, 0.0],
+        [0.1, 0.0, 0.0],
+        [0.0, 0.0, 0.0],
+        500000.0,
+    ));
+
+    for i in 0..1000 {
         system.add_particle(Particle::new(
-            i as u64,
-            [i as f64 * 50.0, 0.0, 0.0],
+            i + 3,
+            [500.26 + (i as f64 * 30.0), 1000.0, 0.0],
+            [0.1, 0.0, 0.0],
             [0.0, 0.0, 0.0],
-            [0.0, 0.0, 0.0],
-            1.5e14 * (i + 1) as f64,
+            -0.0000000000000001,
         ));
     }
 
-    let start = Instant::now();
+    set_fullscreen(true);
 
-    let forces = system.compute_all_forces();
-    println!("{forces:?}");
+    loop {
+        clear_background(BLACK);
 
-    for (i, force) in forces.iter().enumerate().take(particle_count) {
-        let p = system.get_particle_by_index(i).expect("REASON");
-        p.update_particle_euler(*force, 0.1);
-        println!("Velocity result {:?}", p.velocity());
-        println!("     Pos result {:?}", p.pos());
+        let forces = system.compute_all_forces();
+
+        for (i, force) in forces.iter().enumerate().take(particle_count) {
+            let p = system.get_particle_by_index(i).expect("REASON");
+            p.update_particle_euler(*force, 100.0);
+        }
+
+        for i in 0..particle_count {
+            let x_cord = system.get_particle_by_index(i).unwrap().pos()[0];
+            let y_cord = system.get_particle_by_index(i).unwrap().pos()[1];
+            if i == 0 {
+                draw_circle(x_cord as f32, y_cord as f32, 100.0, YELLOW);
+            } else if i == 1 {
+                draw_circle(x_cord as f32, y_cord as f32, 10.0, BLUE);
+            } else if i == 2 {
+                draw_circle(x_cord as f32, y_cord as f32, 5.0, RED);
+            } else if i % 2 > 0 {
+                draw_circle(x_cord as f32, y_cord as f32, 2.0, ORANGE);
+            } else {
+                draw_circle(x_cord as f32, y_cord as f32, 1.0, WHITE);
+            }
+        }
+
+        next_frame().await
     }
-
-    let duration = start.elapsed();
-
-    println!("Total Time: {duration:#?}.");
 }
